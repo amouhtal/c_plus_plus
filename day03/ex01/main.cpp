ccb39605a6ab2f5db4053dded65dcbf371daa65d
Abderrahmane Mouhtal <amouhtal@e1r10p4.1337.ma>
2021-09-30 18:15:36 +0100

    day03

--- day03/ex01/main.cpp
+++ day03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amouhtal <amouhtal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 17:55:20 by amouhtal          #+#    #+#             */
-/*   Updated: 2021/09/30 17:55:22 by amouhtal         ###   ########.fr       */
+/*   Updated: 2021/09/30 18:14:51 by amouhtal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 int main()
 {
 
-    ClapTrap C_instance0;
-    ClapTrap C_instance1("claptrap");
-    ClapTrap C_instance2(C_instance1);
-    C_instance2.attack("target");
-    C_instance2.takeDamage(30);
-    C_instance2.beRepaired(20);
+    // ClapTrap C_instance0;
+    // ClapTrap C_instance1("claptrap");
+    // ClapTrap C_instance2(C_instance1);
+    // C_instance2.attack("target");
+    // C_instance2.takeDamage(30);
+    // C_instance2.beRepaired(20);
 
     std::cout << "------------------------------------------" << std::endl;
 
@@ -28,12 +28,4 @@ int main()
     ScavTrap S_instance1("scavtrap");
     ScavTrap S_instance2(S_instance1);
     S_instance2.guardGate();
-
-    // ClapTrap instance0;
-    // ClapTrap instance1("claptrap");
-    // ClapTrap instance2(instance1);
-
-    // instance2.attack("target");
-    // instance2.takeDamage(30);
-    // instance2.beRepaired(20);
 }
--- day03/ex02/main.cpp
+++ day03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amouhtal <amouhtal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 17:56:25 by amouhtal          #+#    #+#             */
-/*   Updated: 2021/09/30 17:56:30 by amouhtal         ###   ########.fr       */
+/*   Updated: 2021/09/30 18:15:10 by amouhtal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 
 int main()
 {
-    ClapTrap C_instance0;
-    ClapTrap C_instance1("claptrap");
-    ClapTrap C_instance2(C_instance1);
-    C_instance2.attack("target");
-    C_instance2.takeDamage(30);
-    C_instance2.beRepaired(20);
+    // ClapTrap C_instance0;
+    // ClapTrap C_instance1("claptrap");
+    // ClapTrap C_instance2(C_instance1);
+    // C_instance2.attack("target");
+    // C_instance2.takeDamage(30);
+    // C_instance2.beRepaired(20);
 
-    std::cout << "------------------------------------------" << std::endl;
+    // std::cout << "------------------------------------------" << std::endl;
 
-    ScavTrap S_instance0;
-    ScavTrap S_instance1("scavtrap");
-    ScavTrap S_instance2(S_instance1);
-    S_instance2.guardGate();
+    // ScavTrap S_instance0;
+    // ScavTrap S_instance1("scavtrap");
+    // ScavTrap S_instance2(S_instance1);
+    // S_instance2.guardGate();
 
-    std::cout << "------------------------------------------" << std::endl;
+    // std::cout << "------------------------------------------" << std::endl;
     
     FragTrap instance0;
     FragTrap instance1("fragtrap");
